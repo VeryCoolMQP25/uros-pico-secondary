7c142ed55b90d7a773e4dec51b86d5a495eb04eb
Jacob Ellington <cannotilever@protonmail.com>
2024-10-02 15:06:59 -0400

    finish PWM implementation

--- src/actuators.c
+++ src/actuators.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/timer.h"
-#include "hardware/clocks.h"
-#include "quadrature_encoder.pio.h"
 #include "actuators.h"
 #include "uart_logging.h"
 
@@ -49,18 +48,54 @@ static Encoder *init_encoder(uint pinA, uint pinB){
 	return enc;
 }
 
-static void init_motor(int pin, Motor *motor_struct){
+void init_motor(int pin, Motor *motor_struct){
 	motor_struct->pin_num = pin;
 	gpio_set_function(pin, GPIO_FUNC_PWM);
-	uint32_t clk = clock_get_hz(clk_sys);
-	uint32_t div = clk / (20000 * 50);
-	
+	uint slice = pwm_gpio_to_slice_num(pin);
+	motor_struct->slice_num = slice;
+	// configure machinery to operate at 333Hz
+	float div = clock_get_hz(clk_sys) / (TALON_PWM_FREQ * (TALON_PWM_WRAP-1)); // should be ~25
+	pwm_config config = pwm_get_default_config();
+	pwm_config_set_clkdiv(&config, div);
+	pwm_config_set_wrap(&config, TALON_PWM_WRAP);
+	// set PWM to neutral before start
+	pwm_set_gpio_level(pin, TALON_DEADCTR);
+	motor_struct->curpower = 0;
+	// init and start PWM channel
+	pwm_init(slice, &config, true);
+	motor_struct->enc = NULL;
 }
 
-void init_motors(){
-	uart_log(LEVEL_INFO, "Starting motor init");
+void init_motor_with_encoder(int pin, Motor *motor_struct, int enc_pin_A, int enc_pin_B){
+	init_motor(pin, motor_struct);
+	motor_struct->enc = init_encoder(enc_pin_A, enc_pin_B);
+	if (motor_struct->enc == NULL){
+		uart_log(LEVEL_ERROR, "Could not init encoder!");
+	}
+}
+
+// accepts integer power level [-100, 100]
+bool set_motor_power(Motor *motor, int power){
+	if(abs(power) > 100){
+		char buff[40];
+		snprintf(buff, 40, "Requsted motor power %d is invalid! Ignoring.", power);
+		uart_log(LEVEL_WARN, buff);
+		return false;
+	}
+	int setpoint = (TALON_DEADCTR+power*(TALON_FULL_FWD-TALON_DEADCTR));
+	pwm_set_gpio_level(motor->pin_num, setpoint);
+	motor->curpower = power;
+	return true;
+}
+
+void init_all_motors(){
+	uart_log(LEVEL_DEBUG, "Starting Prog. I/O init");
 	// load pio program into both PIOs
 	pio_add_program(pio0, &quadrature_encoder_program);
 	pio_add_program(pio1, &quadrature_encoder_program);
-	
+	uart_log(LEVEL_DEBUG, "Starting motor init");
+	init_motor(DT_L_PWM, &drivetrain_left);
+	init_motor(DT_R_PWM, &drivetrain_right);
+	init_motor(LIFT_PWM, &lift_motor);
+	uart_log(LEVEL_DEBUG, "Motor & Encoder init finished.");
 }
--- src/actuators.h
+++ src/actuators.h
@@ -10,6 +10,24 @@
 #define DT_R_ENCODER_A 8
 #define DT_R_ENCODER_B 9
 
+#define DT_L_PWM 10
+#define DT_R_PWM 11
+#define LIFT_PWM 13
+
+/** Talon SR speed controller specifics
+ * 333Hz signal
+ * 2.037ms = full "forward"
+ * 1.539ms = the "high end" of the deadband range
+ * 1.513ms = center of the deadband range (off)
+ * 1.487ms = the "low end" of the deadband range
+ * 0.989ms = full "reverse"
+*/
+#define TALON_PWM_FREQ 333
+#define TALON_PWM_WRAP 15015
+#define TALON_FULL_FWD 10184
+#define TALON_FULL_REV 4945
+#define TALON_DEADCTR	7565
+
 typedef struct {
 	PIO pio;
 	uint sm; 
@@ -18,9 +36,11 @@ typedef struct {
 typedef struct {
 	uint pin_num;
 	uint slice_num;
+	int curpower;
 	Encoder *enc;
 } Motor;
 
-void init_motors();
+void init_all_motors();
+bool set_motor_power(Motor*, int);
 
 #endif
